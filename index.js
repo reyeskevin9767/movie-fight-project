a3762eb142b06e48ee6b79b9b31039df3a690655
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 19:43:29 -0400

    comparing movies values

--- index.js
+++ index.js
@@ -82,7 +82,27 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 
 //* Compare movies
 const runComparison = () => {
-  console.log('time For Comparison');
+  const leftSideStats = document.querySelectorAll(
+    '#left-summary .notification'
+  );
+  const rightSideStats = document.querySelectorAll(
+    '#right-summary .notification'
+  );
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index];
+
+    const leftSideValue = parseInt(leftStat.dataset.value);
+    const rightSideValue = parseInt(rightStat.dataset.value);
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove('is-primary');
+      leftStat.classList.add('is-warning');
+    } else {
+      rightStat.classList.remove('is-primary');
+      rightStat.classList.add('is-warning');
+    }
+  });
 };
 
 //* Generate HTML for movie's details
