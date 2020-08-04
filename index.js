d68219adadf474342a9c8afe7ad81d916cb182b6
Kevin Reyes <reyeskevin9767@gmail.com>
2020-08-04 16:18:48 -0400

    add title to search bar

--- index.js
+++ index.js
@@ -54,6 +54,11 @@ const onInput = async (event) => {
     ${movie.Title}
     `;
 
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active');
+      input.value = movie.Title;
+    });
+
     resultsWrapper.appendChild(option);
   }
 };
